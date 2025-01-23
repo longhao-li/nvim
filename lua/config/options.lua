8e5a4f0982fe3e4f33d0c97a915eb1100c7a887b
Longhao Li <longhao_li@outlook.com>
2025-01-23 13:28:50 +0800

    Use PowerShell as default shell for Windows.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,11 @@
 vim.opt.relativenumber = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+
+if vim.fn.has("win32") == 1 then
+    if vim.fn.executable("pwsh") == 1 then
+        vim.o.shell = "pwsh"
+    elseif vim.fn.executable("powershell") == 1 then
+        vim.o.shell = "powershell"
+    end
+end
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,4 +1,14 @@
 return {
-    {"ellisonleao/gruvbox.nvim"},
-    {"LazyVim/LazyVim", opts = {colorscheme = "gruvbox"}}
+    { "ellisonleao/gruvbox.nvim" },
+    { "LazyVim/LazyVim", opts = { colorscheme = "gruvbox" } },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                },
+            },
+        },
+    },
 }
